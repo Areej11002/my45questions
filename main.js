4feae968a6b2ab735cf00188e0866c2fe455e331
Areej11002 <160529992+Areej11002@users.noreply.github.com>
2024-02-19 23:17:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// question1
+console.log("hello world");
+// question2
+var myname = "areej";
+var message = "is a good person";
+console.log(myname + " " + message);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// question1
+console.log("hello world");
+
+
+// question2
+let myname = "areej";
+let message = "is a good person";
+console.log(myname+" "+message);
